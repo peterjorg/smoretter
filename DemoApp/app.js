c3722d75212159042be98327489c3a0a2961b761
Peter Jausovec <peterjausovec@users.noreply.github.com>
2015-09-17 10:13:57 -0700

    Removing unused route

--- DemoApp/app.js
+++ DemoApp/app.js
@@ -11,7 +11,6 @@ var redisClient = require('./redis.js');
 
 // Routes
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var login = require('./routes/login');
 var register = require('./routes/register');
 
@@ -38,7 +37,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/login', login);
 app.use('/register', register);
 
